0c148a8b40f0485eca132c23af9222556c6dd13c
Siddhant Agrawal <siddhant_010@outlook.com>
2024-06-12 00:53:09 +0530

    changed logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className=" border-green-200 bg-green-500">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="../public/logo.png" className="h-8" alt="Flowbite Logo" />
+          <img src="../public/logo.svg" className="h-8" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             SpreadSheet Crud-App
           </span>
